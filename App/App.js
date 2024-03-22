5a2bd254d0c9b736489dd387d22b11f0a56b600c
Rodrigo Pereira Couto <57036963+rodrigoptu12@users.noreply.github.com>
2024-03-22 10:38:11 -0300

    save login

--- App/App.js
+++ App/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./src/screens/login";
+import App from "./src/screens/app";
 export default function App() {
   return (
     <View style={styles.container}>
       <Login />
+      <App />
       <StatusBar style="auto" />
     </View>
   );
